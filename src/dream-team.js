50454907ce7999bc11e5121cd9920a5adc112fbe
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-02-16 15:07:37 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,7 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    const newArray = members.filter(item => typeof item === 'string').map(item => item.toUpperCase().replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, ''));
+    const res=newArray.sort().reduce((acc, item) => (acc + item.slice(0,1)), '');
+    return res;
+  } return false;  
 };

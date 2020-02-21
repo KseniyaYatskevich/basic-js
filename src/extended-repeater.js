3f060145519ade6a35cc4c12f40c2c9e446cae6a
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-02-21 14:28:29 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,40 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    const {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} =options;
+    let strNew = str;
+    let additionNew = addition;
+    let repeatStr = '';
+    let separatorNew = '+';
+    let additionStr = '';
+    let additionSeparatorNew = '||';
+    let repeatArray = new Array(repeatTimes);
+    let additionArray = new Array(additionRepeatTimes);
+    if (str === null) {
+        strNew = 'null';
+    }
+    if (addition === null) {
+        additionNew = 'null';
+    }
+    if(options.hasOwnProperty('additionSeparator')) {
+        additionSeparatorNew = additionSeparator;
+    }
+    if(additionArray.length !== 0) {
+        if(additionArray.length === 1) {
+            additionStr = additionArray.fill(additionNew).join('');
+        }
+        additionStr = additionArray.fill(additionNew).join(`${additionSeparatorNew}`);
+    }
+    if(options.hasOwnProperty('separator')) {
+        if(additionStr.length>0) {
+            separatorNew = separator + additionStr;
+        }
+        separatorNew = separator;
+    } 
+    console.log(separatorNew)
+    if(repeatArray.length !== 0) {
+        repeatStr = repeatArray.fill(strNew+additionStr).join(`${separatorNew}`);
+    }
+    console.log(separatorNew)
+    console.log(additionStr, 11111)
+    return repeatStr;
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
+module.exports = function transform(/*arr*/) {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    // if(!Array.isArray(arr) || arr.length === 0) {
+    //     throw 'Not implemented';
+    // }
+    // if (arr.every(item => typeof item==='number')) return arr;
+    // for(let i = 0; i< arr.length; i++) {
+    //     switch(a[i]) {
+    //        case'--discard-next': 
+    //         if(a[i] === '--discard-next') arr.splice(i+1, 1);
+            
+
+    //         }
+    //        case'--discard-prev': if(a[i] === '--discard-prev-next') {
+    //             return arr.splice(i-1, 1);
+    //         }
+    //     }
+    // }
 };

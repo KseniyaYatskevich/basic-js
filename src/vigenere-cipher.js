637ddf0048a28a7359beb8dff46ba5516e5ff3ed
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-03-01 15:54:54 +0300

    feat: add solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,49 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(!message || !key) throw new Error();
+        let messageArray = message.toUpperCase().split('');
+        let keyArray = key.toUpperCase().split('');
+        let result = [];
+        for(let i=0; i<messageArray.length; i++) {
+            let codeLetter = messageArray[i].charCodeAt(0) - 65;
+            if(codeLetter < 0 || codeLetter > 25) {
+                result.push(messageArray[i]);
+            } else {
+
+                let keyLetter = keyArray.shift();
+                let keyCode = keyLetter.charCodeAt(0) - 65;
+                keyArray.push(keyLetter);
+                result.push(String.fromCharCode((codeLetter + keyCode) % 26 + 65));
+            }
+        }
+        if(!this.isDirect) {
+            return result = result.reverse().join('');
+        } else return result.join('');
+    }
+
+    decrypt(encryptedMessage, key) {
+        if(!encryptedMessage || !key) throw Error();
+        let encryptedMessageArray = encryptedMessage.toUpperCase().split('');
+        let keyArray = key.toUpperCase().split('');
+        let result = [];
+        for (let i = 0; i<encryptedMessageArray.length; i++) {
+            let codeLetter = encryptedMessageArray[i].charCodeAt(0) - 65;
+            if (codeLetter < 0 || codeLetter > 25) {
+                result.push(encryptedMessageArray[i]);
+            } else {
+                let keyLetter = keyArray.shift();
+                let keyCode = keyLetter.charCodeAt(0) - 65;
+                keyArray.push(keyLetter);
+                result.push(String.fromCharCode((codeLetter + 26 - keyCode) % 26 + 65));
+            }
+        }
+        if(!this.isDirect) {
+           return  result = result.reverse().join('');
+        } else return result = result.join('');
     }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -2,8 +2,6 @@ module.exports = function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
   if(typeof date === 'object' && !date.hasOwnProperty('getMonth')) {
     const month = date.getMonth();
-    console.log(date.getMonth())
-    console.log(date)
     if(month >=2 && month<5) {
       return 'spring';
     } else if(month >=5 && month<8) {
